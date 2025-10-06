ad12c715918773e2240faeee0ca437d1c66b3cfd
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-10-06 13:55:47 -0300

    iterating pt. 6

---
+++ files/scripts/settheming.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+
+ICON_DIR="$HOME/.local/share/icons"
+THEME_DIR="$HOME/.local/share/themes"
+
+mkdir -p "$ICON_DIR" "$THEME_DIR"
+
+install_colloid_icons() {
+  echo "Installing Colloid Icon Theme..."
+  git clone https://github.com/vinceliuice/Colloid-icon-theme.git /tmp/colloid-icons
+  cd /tmp/colloid-icons
+  ./install.sh -d "$ICON_DIR"
+  cd -
+  rm -rf /tmp/colloid-icons
+  echo "Colloid Icon Theme installed."
+}
+
+install_colloid_gtk() {
+  echo "Instalando Colloid GTK Theme..."
+  git clone https://github.com/vinceliuice/Colloid-gtk-theme.git /tmp/colloid-gtk
+  cd /tmp/colloid-gtk
+  ./install.sh -d "$THEME_DIR"
+  cd -
+  rm -rf /tmp/colloid-gtk
+  echo "Colloid GTK Theme installed."
+}
+
+# Fix for Flatpak
+
+sudo flatpak override --filesystem=xdg-config/gtk-3.0 && sudo flatpak override --filesystem=xdg-config/gtk-4.0
+
+install_bibata_cursor() {
+  echo "Instalando Bibata Cursor Theme..."
+  BIBATA_URL="https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Classic.tar.xz"
+  wget "$BIBATA_URL" -O /tmp/bibata.tar.xz
+  tar -xvf /tmp/bibata.tar.xz -C "$ICON_DIR"
+  rm /tmp/bibata.tar.xz
+  echo "Bibata Cursor installed."
+}
+
+install_colloid_icons
+install_colloid_gtk
+install_bibata_cursor
+
