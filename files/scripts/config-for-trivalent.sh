3e677602c5d0fee71a92a8ad50e234541a1861ad
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-10-07 13:13:25 -0300

    fixing trivalent patches script pt.2

--- files/scripts/config-for-trivalent.sh
+++ files/scripts/config-for-trivalent.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 echo "Installing secureblue Trivalent selinux policy"
 echo "Disable Negativo repo, to prevent installing conflicting non-needed 'schily' package"
-dnf5 -y config-manager setopt fedora-cdrtools.enabled=0
-dnf5 -y config-manager setopt fedora-multimedia.enabled=0
+if [ -f /etc/yum.repos.d/fedora-cdrtools.repo ]; then
+    dnf5 -y config-manager setopt fedora-cdrtools.enabled=0
+fi
+if [ -f /etc/yum.repos.d/fedora-multimedia.repo ]; then
+    dnf5 -y config-manager setopt fedora-multimedia.enabled=0
+fi
 echo "Install 'selinux-policy-devel' build package & it's dependencies"
 dnf5 -y install selinux-policy-devel
 echo "Downloading secureblue Trivalent selinux policy"
@@ -23,8 +27,12 @@ bash "${PWD}/installselinuxpolicies.sh"
 echo "Cleaning up build package 'selinux-policy-devel' & it's dependencies"
 dnf5 -y remove selinux-policy-devel
 echo "Enabling Negativo repos (as default state)"
-dnf5 -y config-manager setopt fedora-cdrtools.enabled=1
-dnf5 -y config-manager setopt fedora-multimedia.enabled=1
+if [ -f /etc/yum.repos.d/fedora-cdrtools.repo ]; then
+    dnf5 -y config-manager setopt fedora-cdrtools.enabled=1
+fi
+if [ -f /etc/yum.repos.d/fedora-multimedia.repo ]; then
+    dnf5 -y config-manager setopt fedora-multimedia.enabled=1
+fi
 
 echo "Assure that network sandbox is always disabled by default (to ensure that login data remains)"
 echo "https://github.com/fedora-silverblue/issue-tracker/issues/603"
