cd6ea500df1b88573fee63d99f5321b5e931fff8
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-10-15 17:54:25 -0300

    trying

--- files/scripts/setsddmtheming.sh
+++ files/scripts/setsddmtheming.sh
@@ -2,9 +2,9 @@
 
 set -oue pipefail
 
-sed -i 's/color: "black"/color: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
-sed -i 's/id: lblPassword/id: lblPassword\ncolor: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
-sed -i 's/id: lblName/id: lblName\ncolor: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
-sed -i 's/id: lblSession/id: lblSession\ncolor: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
-sed -i 's/id: lblLayout/id: lblLayout\ncolor: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
-sed -i 's/id: errorMessage/id: errorMessage\ncolor: "white"/' /usr/share/sddm/themes/puffy-stars/Main.qml
+sed -i 's/color: "black"/color: "white"/' /usr/share/sddm/themes/stars/Main.qml
+sed -i 's/id: lblPassword/id: lblPassword\ncolor: "white"/' /usr/share/sddm/themes/stars/Main.qml
+sed -i 's/id: lblName/id: lblName\ncolor: "white"/' /usr/share/sddm/themes/stars/Main.qml
+sed -i 's/id: lblSession/id: lblSession\ncolor: "white"/' /usr/share/sddm/themes/stars/Main.qml
+sed -i 's/id: lblLayout/id: lblLayout\ncolor: "white"/' /usr/share/sddm/themes/stars/Main.qml
+sed -i 's/id: errorMessage/id: errorMessage\ncolor: "white"/' /usr/share/sddm/themes/stars/Main.qml
