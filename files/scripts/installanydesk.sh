2fff375357cf62d9d28294011c20064b840a47f4
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-10-29 21:26:08 -0300

    trying...

--- files/scripts/installanydesk.sh
+++ files/scripts/installanydesk.sh
@@ -2,21 +2,16 @@
 set -euo pipefail
 
 echo "Adding AnyDesk repository..."
-cat > /etc/yum.repos.d/AnyDesk-Fedora.repo << 'EOF'
+cat > /etc/yum.repos.d/AnyDesk-Fedora.repo << "EOF" 
 [anydesk]
 name=AnyDesk Fedora - stable
 baseurl=http://rpm.anydesk.com/fedora/$basearch/
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY
-enabled=1
-priority=90
 EOF
 
-echo "Importing AnyDesk GPG key..."
-rpm --import https://keys.anydesk.com/repos/RPM-GPG-KEY
-
 echo "Installing AnyDesk..."
-rpm-ostree install anydesk
+dnf5 install -y anydesk
 
 echo "AnyDesk installation completed."
